6ae98145e0fbe45376114b70330c5465c021fed4
Alexlg <Alejandrolg86@gmail.com>
2022-10-11 19:54:51 +0200

    creating new tests

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ const express = require('express');
 
 const app = express();
 
+// Config express
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.send('Todo ok amigos');
-});
+app.use(require('./routes'));
 
 module.exports = app;
--- test/api/trips.route.spec.js
+++ test/api/trips.route.spec.js
@@ -1,7 +1,16 @@
 const request = require('supertest');
-const app = require('../../app')
+const mongoose = require('mongoose');
+const app = require('../../app');
+const Trip = require('../../models/trip.model');
 describe('Pruebas sobre la Api de trips', () => {
 
+    beforeAll(async () => {
+        await mongoose.connect('mongodb://127.0.0.1/familyTrips');
+    });
+
+    afterAll(async () => {
+        await mongoose.disconnect();
+    });
     describe('GET /api/trips', () => {
 
         let response
@@ -18,6 +27,34 @@ describe('Pruebas sobre la Api de trips', () => {
 
         it('La petición nos devuelve un array de trips', async () => {
             expect(response.body).toBeInstanceOf(Array);
+        })
+    });
+
+    describe('POST /api/trips', () => {
+
+        const newTrip = {
+            name: 'test trips',
+            destination: 'Berlin',
+            category: 'familiar',
+            start_date: '2022-06-20'
+        };
+
+        afterAll(async () => {
+            await Trip.deleteMany({ name: 'test trips' });
+        });
+
+        it('La ruta funcione', async () => {
+            const response = await request(app).post('/api/trips').send(newTrip);
+
+            expect(response.status).toBe(200);
+            expect(response.headers['content-type']).toContain('json');
+        });
+
+        it('Se inserta correctamente', async () => {
+            const response = await request(app).post('/api/trips').send(newTrip);
+
+            expect(response.body._id).toBeDefined();
+            expect(response.body.name).toBe(newTrip.name);
         })
     });
 });
